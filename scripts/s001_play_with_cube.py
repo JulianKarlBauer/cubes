79e3bd6e0fe3833942bdf7b4766fcacfa4854190
Julian Karl Bauer <juliankarlbauer@gmx.de>
2024-07-10 21:36:15 +0200

    Simplify

--- scripts/s001_play_with_cube.py
+++ scripts/s001_play_with_cube.py
@@ -10,22 +10,6 @@ print(f"cube.is_done() = {cube.is_done()}")
 ##################
 
 rotations = "R' L2 U D' F B'2 R' L"
-print(f"cube.rotate({rotations})")
-cube.rotate(rotations)
-print(cube)
-
-print("cube.scramble()")
-print(cube)
-
-print(f"cube.history() = {cube.history()}")
-
-print("cube.reset()")
-cube.reset()
-print(cube)
-
-print(f"cube.history() = {cube.history()}")
-
-##################
 
 print(f"cube.rotate({rotations})")
 cube.rotate(rotations)
@@ -34,7 +18,7 @@ print(f"cube.history()= {cube.history()}")
 
 print(f"cube.reverse_history(to_str=True) = {cube.reverse_history(to_str=True)}")
 
-print(f"cube.rotate(cube.reverse_history(to_str=True))")
+print(f"cube.rotate(cube.reverse_history())")
 cube.rotate(cube.reverse_history())
 # cube.rotate("L' R B2 F' D U' L'2 R")
 print(cube)
