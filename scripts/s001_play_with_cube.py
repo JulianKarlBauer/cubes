8d805f322f00a5095ee04e50890b04fe5eae456d
Julian Karl Bauer <juliankarlbauer@gmx.de>
2024-07-10 20:45:28 +0200

    Use local clone of magiccube which fixed the reverse bug

--- scripts/s001_play_with_cube.py
+++ scripts/s001_play_with_cube.py
@@ -30,26 +30,11 @@ print(f"cube.history() = {cube.history()}")
 print(f"cube.rotate({rotations})")
 cube.rotate(rotations)
 print(cube)
+print(f"cube.history()= {cube.history()}")
 
 print(f"cube.reverse_history(to_str=True) = {cube.reverse_history(to_str=True)}")
 
 print(f"cube.rotate(cube.reverse_history(to_str=True))")
-# cube.rotate(cube.reverse_history())
-cube.rotate("L' R B2 F' D U' L'2 R")
+cube.rotate(cube.reverse_history())
+# cube.rotate("L' R B2 F' D U' L'2 R")
 print(cube)
-
-# # Scramble the cube
-# cube.scramble()
-
-# # Print the move history
-# print("History: ", cube.history())
-
-# # Print the moves to reverse the cube history
-# cube.rotate(cube.reverse_history())
-
-# # Check that the cube is done
-# assert cube.is_done()
-
-# # Print the cube
-# print("Solved Cube")
-# print(cube)
